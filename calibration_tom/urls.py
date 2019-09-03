55ffa96edcb34ffe0e74768c6c63c826fcfd975c
Lindy Lindstrom <llindstrom@lco.global>
2019-09-03 16:04:57 -0700

    add staticfile_urlpatterns() to urlpatterns
    this is neccessary when running via gnunicorn in a
    docker container. it's not necessary, but doesn't hurt,
    when running via ./manage.py runserver. probably not suitable
    for production deployment.

--- calibration_tom/urls.py
+++ calibration_tom/urls.py
@@ -14,7 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', include('tom_common.urls')),
 ]
+
+# this is to serve static file from a debug Dockerfile environment
+# this is perform a findstatic
+urlpatterns += staticfiles_urlpatterns()

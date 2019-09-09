bd62236bbb63a51ff9ddb676a3004c9987964369
Lindy Lindstrom <llindstrom@lco.global>
2019-09-09 09:46:01 -0700

    put calibrations index page in TOM template hierarchy

--- calibrations/views.py
+++ calibrations/views.py
@@ -1,6 +1,11 @@
-# from django.shortcuts import render
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
 def index(request):
+    context = {}
+    return render(request, 'calibrations/index.html', context)
+
+
+def index1(request):
     return HttpResponse(f"this is the beginning of the calibrations app: {request}")

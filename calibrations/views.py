4ed3a139fd663e20fb97f0269f37659aed415041
Lindy Lindstrom <llindstrom@lco.global>
2019-09-11 10:38:18 -0700

    Experimenting with various lists of things (wip)

--- calibrations/views.py
+++ calibrations/views.py
@@ -3,9 +3,13 @@ from typing import Dict, List
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
+from .models import Instrument, Site
+
 
 def index(request: HttpRequest) -> HttpResponse:
     context: Dict[str, List] = {
-        'stuff_to_list': ['one', 'two', 'three', 'four']
+        'stuff_to_list': ['one', 'two', 'three', 'four'],
+        'sites': Site.objects.all(),
+        'instruments': Instrument.objects.all(),
     }
     return render(request, 'calibrations/index.html', context)

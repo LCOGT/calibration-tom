5a732045ac7e9e23f4d5a8bde204cdd20b4466b7
Lindy Lindstrom <llindstrom@lco.global>
2019-09-11 09:23:54 -0700

    register calibrations/models classes with admin

--- calibrations/admin.py
+++ calibrations/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Site
+from .models import Enclosure
+from .models import Telescope
+from .models import Camera
+from .models import CameraType
+from .models import FilterWheel
+from .models import OpticalElementGroup
+from .models import Instrument
+
+admin.site.register(Site)
+admin.site.register(Enclosure)
+admin.site.register(Telescope)
+admin.site.register(Camera)
+admin.site.register(CameraType)
+admin.site.register(FilterWheel)
+admin.site.register(OpticalElementGroup)
+admin.site.register(Instrument)
+
+
+

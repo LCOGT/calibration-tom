5f4bfd5fc0d7d1ed3787a005fc3d0dfa06beb258
Lindy Lindstrom <llindstrom@lco.global>
2019-09-11 09:25:14 -0700

    demonstrate simple context passing/list processing
    After passing a list to a template via the context,
    the template first checks to see if the stuff_to_list
    key is in the context and, if so, displays the key-value
    list as in a <ul><li> unordered list.

--- calibrations/views.py
+++ calibrations/views.py
@@ -1,9 +1,11 @@
-from typing import Dict
+from typing import Dict, List
 
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    context: Dict[str, str] = {}
+    context: Dict[str, List] = {
+        'stuff_to_list': ['one', 'two', 'three', 'four']
+    }
     return render(request, 'calibrations/index.html', context)

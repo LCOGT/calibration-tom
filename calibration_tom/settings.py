619b777eaee711adf542c49cdfa416af4a5de5a4
Lindy Lindstrom <llindstrom@lco.global>
2020-07-29 23:17:36 +0000

    fix collectstatic problems with whitenoise

--- calibration_tom/settings.py
+++ calibration_tom/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*', ]
 # Application definition
 
 INSTALLED_APPS = [
+    'whitenoise.runserver_nostatic',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -61,6 +62,7 @@ SITE_ID = 1
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',

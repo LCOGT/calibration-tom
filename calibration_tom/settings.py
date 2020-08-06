54819ac9ad76564bfe2a8b355ddfd539c628d65f
Lindy Lindstrom <llindstrom@lco.global>
2020-08-06 20:20:46 +0000

    define a default set of data products and processors
    When tom_setup was run in Fall'19 empty DATA_PRODUCT_TYPES
    was ok, but not anymore appearently b/c there was a KeyError
    (photometry) when displaying target detail page.

--- calibration_tom/settings.py
+++ calibration_tom/settings.py
@@ -201,8 +201,19 @@ FACILITIES = {
     }
 }
 
-# TODO: Will any be necessary?
-DATA_PRODUCT_TYPES = {}
+# Define the valid data product types for your TOM. Be careful when removing items, as previously valid types will no
+# longer be valid, and may cause issues unless the offending records are modified.
+DATA_PRODUCT_TYPES = {
+    'photometry': ('photometry', 'Photometry'),
+    'fits_file': ('fits_file', 'FITS File'),
+    'spectroscopy': ('spectroscopy', 'Spectroscopy'),
+    'image_file': ('image_file', 'Image File')
+}
+
+DATA_PROCESSORS = {
+    'photometry': 'tom_dataproducts.processors.photometry_processor.PhotometryProcessor',
+    'spectroscopy': 'tom_dataproducts.processors.spectroscopy_processor.SpectroscopyProcessor',
+}
 
 TOM_ALERT_CLASSES = []
 

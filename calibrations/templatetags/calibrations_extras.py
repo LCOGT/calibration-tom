ac2e48e151dcbb034506444eca6dcf36d4d2c662
David Collom <dcollom@lco.global>
2020-10-01 13:14:55 -0700

    Updated target data for seasonal start and end and added simpletag to display months

--- calibrations/templatetags/calibrations_extras.py
+++ calibrations/templatetags/calibrations_extras.py
@@ -9,3 +9,9 @@ def site_calibration_button(site, instrument):
     Renders a button for creating a new series of calibrations for a given instrument/site.
     """
     return {'site': site, 'instrument': instrument}
+
+
+@register.simple_tag
+def display_seasonal_start_or_end(month):
+    return month.strftime('%B')
+    # return datetime(year=datetime.now().year, month=month, day=1).strftime('%B')

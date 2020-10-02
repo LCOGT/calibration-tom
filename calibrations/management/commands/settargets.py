c638edec9f49a5dd1894164c3b43b46bd27eaac1
David Collom <dcollom@lco.global>
2020-10-02 09:12:37 -0700

    potentially solved target selection problems

--- calibrations/management/commands/settargets.py
+++ calibrations/management/commands/settargets.py
@@ -1,20 +1,15 @@
+import calendar
+from datetime import datetime, timedelta
+import random
+
 from django.core.management.base import BaseCommand
-from django.db.models import Q
+from django.db.models import DateTimeField, ExpressionWrapper, F
 
 from tom_targets.models import Target, TargetExtra
 from tom_observations.models import DynamicCadence, ObservationGroup, ObservationRecord
 
 
-DynamicCadence.objects.create(
-    observation_group=og,
-    cadence_strategy='NRESCalibrationCadence',
-    active=True,
-    cadence_parameters={
-        'site': 'cpt',
-        'target_id': 1,
-        'period': 120
-    }
-)
+CADENCE_DURATION = 3
 
 
 class Command(BaseCommand):
@@ -27,28 +22,115 @@ class Command(BaseCommand):
     def add_arguments(self, parser):
         parser.add_argument('-s', '--site', help=self.sites, required=True)
 
+    def get_eligible_targets(self, target_ids_to_exclude=[]):
+        """
+        Gets targets eligible to be selected for the next cadence window
+
+        :param target_ids_to_exclude: List of target IDs to omit from the result
+        :type target_ids_to_exclude: list of ints
+        """
+
+        now = datetime.now()
+        cadence_end_datestamp = now + timedelta(month=CADENCE_DURATION)
+
+        eligible_targets = []
+        for target in Target.objects.exclude(
+                                        id__in=target_ids_to_exclude
+                                    ).filter(
+                                        targetextra__key='nres_active_target', value=True
+                                    ):
+            # TODO: validate that there's only one of each TargetExtra
+            target_seasonal_start = target.targetextra_set.filter(key='seasonal_start').first().float_value
+            seasonal_start_year = now.year if now.month >= target_seasonal_start else now.year - 1
+            seasonal_start_date = datetime(year=seasonal_start_year, month=target_seasonal_start, day=1)
+
+            target_seasonal_end = target.targetextra_set.filter(key='seasonal_end').first().float_value
+            seasonal_end_year = now.year if now.month <= target_seasonal_end else now.year + 1
+            visible_window_length = datetime(
+                year=seasonal_end_year,
+                month=target_seasonal_end,
+                day=calendar.monthrange(cadence_end_datestamp.year, target_seasonal_end)
+            )[1] - now
+
+            if seasonal_start_date <= now and visible_window_length > CADENCE_DURATION:
+                eligible_targets.append(target)
+
+        return eligible_targets
+
+    def get_new_target(self, target_ids_to_exclude=[]):
+        """
+        Selects a target visible at the given site that will be visible for the next three months.
+
+        :param target_ids_to_exclude: List of target IDs to omit from the result
+        :type target_ids_to_exclude: list of ints
+        """
+
+        # seasonal_start must be in current month or prior
+        # remaining time in the visibility window > CADENCE_DURATION
+        # remaining time = seasonal_end - now >= CADENCE_DURATION
+
+        # visible_window_length = ExpressionWrapper(
+        #     datetime(year=now.year if now.month <= F('seasonal_end') else now.year + 1,
+        #              month=F('seasonal_end'),
+        #              day=calendar.monthrange(cadence_end_datestamp.year, F('seasonal_end'))[1]
+        #              ) - datetime.now()
+        # )
+
+        # seasonal_end_date = ExpressionWrapper(
+        #     datetime(year=now.year if now.month <= F('seasonal_end') else now.year + 1,
+        #              month=F('seasonal_end'),
+        #              day=calendar.monthrange(cadence_end_datestamp.year, F('seasonal_end'))[1]
+        #              )
+        # )
+
+        # current date 6/1/2020
+        # seasonal_end 11/30/2020 -> should be visible window length of 5 months
+        # CADENCE_DURATION 7 months
+        # 11/30/2021 -> actual visible window length = 17 months
+        # should be invalid target, becomes eligible target
+
+        # seasonal_end_dates = TargetExtra.objects.filter(key='seasonal_end')
+
+        # eligible_targets = Target.objects.exclude(
+        #                                     targets_to_exclude
+        #                                 ).filter(
+        #                                     targetextra__in=TargetExtra.objects.filter(key='nres_active_target', value=True)
+        #                                 ).filter(
+        #                                     targetextra__in=TargetExtra.objects.filter(key='seasonal_end')   
+        #                                 )
+
+        eligible_targets = self.get_eligible_targets(target_ids_to_exclude=target_ids_to_exclude)
+
+        return eligible_targets[random.randint(0, len(eligible_targets)-1)]
+
     def handle(self, *args, **options):
         # Identify and get the correct DynamicCadence for a site
-        target_for_site = Target.objects.filter(
-            targetextra__in=Q(TargetExtra.objects.filter(key='site', value=options['site']) |
-                              TargetExtra.objects.filter(key='nres_active_target', value=True))
-        ).first() # This assumes that there's only one
-        most_recent_obs_for_target = ObservationRecord.objects.filter(target=target_for_site).order_by('-created').first()
-        observation_group_for_cadence = ObservationGroup.objects.filter(observation_set__contains=most_recent_obs_for_target, dynamiccadence_set__contains(DynamicCadence.objects.filter(cadence_strategy='NRESCalibrationCadence'))).first()
-
-        DynamicCadence.objects.filter(cadence_parameters__site='cpt', active=True).observation_group
-
-        old_dynamic_cadence = DynamicCadence.objects.first()
+        # TODO: add "last_run" as a property for DynamicCadence
+        cadence_for_site = DynamicCadence.objects.filter(cadence_parameters__site=options['site'], active=True)
+        if len(cadence_for_site) == 0:
+            raise Exception(f"No active cadence was found for site {options['site']}")
+        elif len(cadence_for_site) > 1:
+            raise Exception(f"More than one active cadence was found for site {options['site']}")
+        else:
+            cadence_for_site = cadence_for_site.first()
 
         # Check if the cadence is still valid
+        # Check if the first run of the cadence was less than 3 months ago
+        cadence_expired = (datetime.now - cadence_for_site.created) >= timedelta(month=CADENCE_DURATION)
 
         # If the cadence is still valid, do nothing
-        
-        # If the cadence is no longer valid:
-        new_dynamic_cadence = DynamicCadence.objects.create(
-            cadence_strategy=old_dynamic_cadence,
-            cadence_parameters={'target_id': new_target.id},
-            observation_group=old_dynamic_cadence.observation_group, active=True
-        )
-        old_dynamic_cadence.active = False
-        old_dynamic_cadence.save()
+        if not cadence_expired:
+            pass
+        else:
+            # If the cadence is no longer valid:
+            # Find next target and create new cadence
+            old_target = cadence_for_site.observation_group.observation_set.first().target
+            new_target = self.get_new_target([old_target.id])
+            new_dynamic_cadence = DynamicCadence.objects.create(
+                cadence_strategy=cadence_for_site.cadence_strategy,
+                cadence_parameters={'target_id': new_target.id},
+                observation_group=cadence_for_site.observation_group,
+                active=True
+            )
+            cadence_for_site.active = False
+            cadence_for_site.save()
--- calibrations/tests.py
+++ calibrations/tests.py
@@ -1,3 +1,42 @@
 from django.test import TestCase
 
-# Create your tests here.
+from tom_targets.models import Target
+
+targets = [
+    ('GJ699', 269.45, 4.69, 4, 8),
+    ('GJ2066', 124.033256, 1.30257423, 12, 3),
+    ('HD4628', 12.09573477, 5.28061377, 8, 12),
+    ('HD16160', 39.0204266, 6.88678655, 9, 1),
+    ('HD36395', 82.86414936, -3.67722821, 10, 2),
+    ('HD76151', 133.5747791, -5.43445736, 12, 3),
+    ('HD103631', 179.0052609, -1.44236244, 1, 6),
+    ('HD12053', 215.8136865, 1.24156716, 3, 8),
+    ('HD148513', 247.1415858, 0.66499963, 4, 8),
+    ('HD152391', 253.2450106, -0.02642119, 4, 8),
+    ('HD173818', 281.863544, -3.63983342, 5, 10),
+    ('HD210988', 333.5596737, -5.74625853, 7, 11),
+    ('HR9087', 0.45603553, -3.02750263, 6, 1),
+    ('HR1544', 72.65301243, 8.90018037, 9, 2),
+    ('HR3454', 130.8061458, 3.39866297, 12, 4),
+    ('HR5501', 221.375851, 0.71727486, 3, 7),
+    ('HR7596', 298.6866567, 0.27362796, 5, 10),
+]
+
+
+class TestTargetSelection(TestCase):
+    def setUp(self):
+        self.targets = []
+        for target in targets:
+            self.targets.append(Target.objects.create(
+                name=target[0], type='SIDEREAL', ra=target[1],
+                dec=target[2], seasonal_start=target[3], seasonal_end=target[4]
+            ))
+
+    def test_get_eligible_targets(self):
+        pass
+
+        # valid seasonal end, current year
+        # invalid seasonal end, current year, after current month
+        # invalid seasonal end, current year, before current month
+        # valid seasonal end, next year
+        # invalid seasonal end, next year

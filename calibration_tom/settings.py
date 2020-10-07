0e1dd7c1830f1147b52a809410323cf2f18f64e8
David Collom <dcollom@lco.global>
2020-10-06 23:40:45 -0700

    Managed to commit my settings again

--- calibration_tom/settings.py
+++ calibration_tom/settings.py
@@ -101,22 +101,15 @@ WSGI_APPLICATION = 'calibration_tom.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
-#        'NAME': os.getenv('DB_NAME', 'tom_demo'),
-#        'USER': os.getenv('DB_USER', 'postgres'),
-#        'PASSWORD': os.getenv('DB_PASS', ''),
-#        'HOST': os.getenv('DB_HOST', '127.0.0.1'),
-#        'PORT': os.getenv('DB_PORT', '5432'),
-#    },
-# }
-
 DATABASES = {
    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+       'ENGINE': os.getenv('DB_ENGINE', 'django.db.backends.postgresql'),
+       'NAME': os.getenv('DB_NAME', 'tom_demo'),
+       'USER': os.getenv('DB_USER', 'postgres'),
+       'PASSWORD': os.getenv('DB_PASS', ''),
+       'HOST': os.getenv('DB_HOST', '127.0.0.1'),
+       'PORT': os.getenv('DB_PORT', '5432'),
+   },
 }
 
 # Password validation

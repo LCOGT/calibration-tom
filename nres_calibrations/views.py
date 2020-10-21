59bce8c9b8c55d1cf536ae3b7df605238a5b2661
Lindy Lindstrom <llindstrom@lco.global>
2020-10-21 21:39:12 +0000

    add canned context data and list/detail navigation links

--- nres_calibrations/views.py
+++ nres_calibrations/views.py
@@ -6,11 +6,29 @@ from django.views.generic import TemplateView
 class NRESCalibrationsIndex(TemplateView):
     template_name = 'nres_calibrations/index.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # add to the context here
+        context['kwargs'] = kwargs  # TODO: remove after debugging
+        return context
+
 
 class NRESInstrumentClassListView(TemplateView):
     """list of instruments for an instrument class
     """
     template_name = 'nres_calibrations/instrument_list.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # add to the context here
+        context['kwargs'] = kwargs  # TODO: remove after debugging
+        context['instruments'] = [
+            {'name': 'nres01'},
+            {'name': 'nres02'},
+            {'name': 'nres03'},
+            {'name': 'nres04'},
+        ]
+        return context
 
 
 class NRESInstrumentTargetListView(TemplateView):
@@ -18,8 +36,38 @@ class NRESInstrumentTargetListView(TemplateView):
     """
     template_name = 'nres_calibrations/target_list.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # add to the context here
+        context['kwargs'] = kwargs  # TODO: remove after debugging
+        context['inst'] = kwargs['inst']
+        context['targets'] = [
+            {'id': 0,
+             'name': 'target00',
+             'active': False,
+             'type': 'RV',
+             },
+            {'id': 1,
+             'name': 'target01',
+             'active': True,
+             'type': 'RV',
+             },
+            {'id': 2,
+             'name': 'target02',
+             'active': False,
+             'type': 'FLUX',
+             },
+        ]
+        return context
+
 
 class NRESInstrumentTargetDetailView(TemplateView):
     """Details of nres calibration target.
     """
     template_name = 'nres_calibrations/target_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # add to the context here
+        context['kwargs'] = kwargs  # TODO: remove after debugging
+        return context

b77a75f3f8cde09f4e5100431f57aa85d33f0876
Lindy Lindstrom <llindstrom@lco.global>
2020-11-12 21:22:52 +0000

    add stubs for bias, dark, flat calibration pages

--- calibrations/urls.py
+++ calibrations/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
-
+from django.views.generic import TemplateView
 from calibrations.views import CalibrationSubmissionView
+
+app_name = 'calibrations'
 
 urlpatterns = [
     path('', CalibrationSubmissionView.as_view(), name='calibrations_index'),
-    # path('floyds_ogg', floyds_ogg, name='floyds_ogg'),
-    # path('floyds_coj', floyds_coj, name='floyds_coj')
+    path('bias/', TemplateView.as_view(template_name="calibrations/bias_stub.html"), name='bias_home'),
+    path('dark/', TemplateView.as_view(template_name="calibrations/dark_stub.html"), name='dark_home'),
+    path('flat/', TemplateView.as_view(template_name="calibrations/flat_stub.html"), name='flat_home'),
 ]

1fe8a9ac73a8ff17bc4a95a1a23e0aa0d4fe9626
Lindy Lindstrom <llindstrom@lco.global>
2020-11-30 22:59:00 +0000

    add various TODOs

--- configdb/tests/test_configdb_connections.py
+++ configdb/tests/test_configdb_connections.py
@@ -8,6 +8,8 @@ import responses
 from configdb.configdb_connections import ConfigDBInterface # the module under test
 from configdb.state import  InstrumentState
 
+# TODO: timestamp the configdb json dump so we know how old it is.
+# TODO: update data/sites.json with latest configdb data.
 _configdb_sites = os.path.join(os.path.dirname(__file__), 'data/sites.json')
 
 f = open(_configdb_sites, 'r')
@@ -16,8 +18,11 @@ f.close()
 
 
 class ConfigDbInterfaceTests(unittest.TestCase):
+
     @responses.activate
     def test_should_not_contain_disabled_instruments(self):
+        """TODO: document me
+        """
         config_db_url = 'http://some-url'
 
         responses.add(responses.GET, f'{config_db_url}/sites/', json=sites, status=HTTPStatus.OK)
@@ -32,6 +37,8 @@ class ConfigDbInterfaceTests(unittest.TestCase):
                 assert state != InstrumentState.DISABLED
 
     def test_should_include_instrument_commissioning(self):
+        """TODO: document me
+        """
         include_instrument = ConfigDBInterface.should_include_instrument(InstrumentState.COMMISSIONING, True, True)
         self.assertTrue(include_instrument)
 
@@ -42,10 +49,14 @@ class ConfigDbInterfaceTests(unittest.TestCase):
         self.assertTrue(include_instrument)
 
     def test_should_not_include_instrument_commissioning(self):
+        """TODO: document me
+        """
         include_instrument = ConfigDBInterface.should_include_instrument(InstrumentState.COMMISSIONING, False, False)
         self.assertFalse(include_instrument)
 
     def test_should_include_instrument_disabled(self):
+        """TODO: document me
+        """
         include_instrument = ConfigDBInterface.should_include_instrument(InstrumentState.DISABLED, True, True)
         self.assertTrue(include_instrument)
 
@@ -53,6 +64,8 @@ class ConfigDbInterfaceTests(unittest.TestCase):
         self.assertTrue(include_instrument)
 
     def test_should_not_include_instrument_disabled(self):
+        """TODO: document me
+        """
         include_instrument = ConfigDBInterface.should_include_instrument(InstrumentState.DISABLED, False, True)
         self.assertFalse(include_instrument)
 
@@ -60,6 +73,8 @@ class ConfigDbInterfaceTests(unittest.TestCase):
         self.assertFalse(include_instrument)
 
     def test_should_include_instrument_manual(self):
+        """TODO: document me
+        """
         include_instrument = ConfigDBInterface.should_include_instrument(InstrumentState.MANUAL, True, True)
         self.assertTrue(include_instrument)
 
@@ -67,6 +82,8 @@ class ConfigDbInterfaceTests(unittest.TestCase):
         self.assertTrue(include_instrument)
 
     def test_should_not_include_instrument_manual(self):
+        """TODO: document me
+        """
         include_instrument = ConfigDBInterface.should_include_instrument(InstrumentState.MANUAL, False, False)
         self.assertFalse(include_instrument)
 
@@ -74,6 +91,8 @@ class ConfigDbInterfaceTests(unittest.TestCase):
         self.assertFalse(include_instrument)
 
     def test_should_include_instrument_schedulable(self):
+        """TODO: document me
+        """
         include_instrument = ConfigDBInterface.should_include_instrument(InstrumentState.SCHEDULABLE, True, True)
         self.assertTrue(include_instrument)
 
@@ -87,6 +106,8 @@ class ConfigDbInterfaceTests(unittest.TestCase):
         self.assertTrue(include_instrument)
 
     def test_should_include_instrument_standby(self):
+        """TODO: document me
+        """
         include_instrument = ConfigDBInterface.should_include_instrument(InstrumentState.STANDBY, True, True)
         self.assertTrue(include_instrument)
 

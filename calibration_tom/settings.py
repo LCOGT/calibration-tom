f7a064949c949530076f715082538f162695afb9
Lindy Lindstrom <llindstrom@lco.global>
2020-12-16 23:15:32 +0000

    define NRES RV data dataprocess and set up obs state change hook

--- calibration_tom/settings.py
+++ calibration_tom/settings.py
@@ -213,6 +213,7 @@ FACILITIES = {
 # Define the valid data product types for your TOM. Be careful when removing items, as previously valid types will no
 # longer be valid, and may cause issues unless the offending records are modified.
 DATA_PRODUCT_TYPES = {
+    'nres_rv': ('nres_rv', 'NRES RV'),
     'photometry': ('photometry', 'Photometry'),
     'fits_file': ('fits_file', 'FITS File'),
     'spectroscopy': ('spectroscopy', 'Spectroscopy'),
@@ -220,6 +221,7 @@ DATA_PRODUCT_TYPES = {
 }
 
 DATA_PROCESSORS = {
+    'nres_rv': 'calibrations.processors.NRESRVDataProcessor',
     'photometry': 'tom_dataproducts.processors.photometry_processor.PhotometryProcessor',
     'spectroscopy': 'tom_dataproducts.processors.spectroscopy_processor.SpectroscopyProcessor',
 }
--- calibrations/hooks.py
+++ calibrations/hooks.py
@@ -1,21 +1,35 @@
 import logging
 
+from tom_observations.facility import get_service_class
+from tom_observations.models import ObservationRecord
+from tom_dataproducts.models import DataProduct, ReducedDatum
+from tom_dataproducts.data_processor import run_data_processor
+
 logger = logging.getLogger(__name__)
 
 
-def observation_change_state(observation, previous_state):
+def observation_change_state(observation: ObservationRecord, previous_state):
     logger.info('Observation change state hook: %s from %s to %s', observation, previous_state, observation.status)
 
     # NRES RV calibration observation state change.
     # NOTE: this could be extracted into a method if desired
 
-    facility_for_observation = observation  # TODO: get facility from observation
     if observation.status == 'COMPLETED':
-        # TODO: 1 create DataProduct
-        # create DataProduct for observation - this will trigger a FITS download from the archive
+        # the LCO facility knows about the data associated with this observation
+        facility_class = get_service_class(observation.facility)
+        # ask the facility for the data
+        data_products = facility_class.save_data_products(observation_record=observation)
 
-        # TODO: 2 trigger spectroscopic_processing
-        # trigger spectroscopic_processing to create a ReducedDatum record
+        # run a data processor on each data product (creates ReducedDatums)
+        for data_product in data_products:
+            logger.info(f'processing {data_product}')
+            # determine and set the data_product_type.
+            data_product.data_product_type = 'nres_rv'
+            # this specifies the processor according to settings.DATA_PRODUCT_TYPES
+            reduced_datums = run_data_processor(data_product)
 
-        pass
+        # TODO 3 clean up the FITS file
+        # remove FITS file b/c all data needed is in the ReducedDatum record
+        # TODO 3a dissociate FITS file from data_product
+        # TODO 3b remove FITS file
 

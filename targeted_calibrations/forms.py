f534583fb7a091ae62b200d67d2c1c4d7567ef6f
Lindy Lindstrom <llindstrom@lco.global>
2021-03-06 02:08:24 +0000

    remove target_is_in_season to models.py for improved organization

--- targeted_calibrations/forms.py
+++ targeted_calibrations/forms.py
@@ -9,19 +9,6 @@ from django.urls import reverse
 from tom_targets.models import Target
 
 
-def target_is_in_season(self, query_date: datetime=datetime.utcnow()):
-    """"Returns True if query_date is between target's seasonal_start and seasonal_end
-    Note: seasonal_start and seasonal_end are month numbers (1=January, etc).
-
-    This method will be added to the Target class with setattr (that's why it has a self argument).
-    """
-    seasonal_start = int(self.targetextra_set.filter(key='seasonal_start').first().value)
-    seasonal_end = int(self.targetextra_set.filter(key='seasonal_end').first().value)
-
-    return seasonal_start <= query_date.month <= seasonal_end
-setattr(Target, 'target_is_in_season', target_is_in_season)  # noqa - add method to Target class
-
-
 class NRESCalibrationSubmissionForm(forms.Form):
     site = forms.ChoiceField(required=True,
                              choices=[('all', 'All Sites')] + [(site, site) for site in settings.NRES_SITES],

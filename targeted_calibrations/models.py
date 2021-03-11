95cf8cc3181cceb04e7166712ae14f844a2d47e1
Lindy Lindstrom <llindstrom@lco.global>
2021-03-11 00:43:55 +0000

    grab number (not text field from targetextras for comparison

--- targeted_calibrations/models.py
+++ targeted_calibrations/models.py
@@ -11,8 +11,8 @@ def target_is_in_season(self, query_date: datetime = datetime.utcnow()):
 
     This method will be added to the Target class with setattr (that's why it has a self argument).
     """
-    seasonal_start = int(self.targetextra_set.filter(key='seasonal_start').first().value)
-    seasonal_end = int(self.targetextra_set.filter(key='seasonal_end').first().value)
+    seasonal_start = int(self.targetextra_set.filter(key='seasonal_start').first().float_value)
+    seasonal_end = int(self.targetextra_set.filter(key='seasonal_end').first().float_value)
 
     return seasonal_start <= query_date.month <= seasonal_end
 setattr(Target, 'target_is_in_season', target_is_in_season)  # noqa - add method to Target class

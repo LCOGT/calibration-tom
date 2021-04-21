499b9be3d070e4691e7e09dc87aa834d79f050b5
Lindy Lindstrom <llindstrom@lco.global>
2021-04-21 21:55:21 +0000

    photometric stds need Filter model to store exposure time and count

--- targeted_calibrations/models.py
+++ targeted_calibrations/models.py
@@ -1,5 +1,7 @@
 from datetime import datetime
 
+from django.db import models
+
 from tom_targets.models import Target
 
 
@@ -16,3 +18,28 @@ def target_is_in_season(self, query_date: datetime = datetime.utcnow()):
 
     return seasonal_start <= query_date.month <= seasonal_end
 setattr(Target, 'target_is_in_season', target_is_in_season)  # noqa - add method to Target class
+
+
+class Filter(models.Model):
+    name = models.CharField(
+        max_length=100, default='', verbose_name='Name', help_text='Name of the filter',
+        unique=True
+    )
+
+    exposure_time = models.FloatField(
+        verbose_name='Exposure Time', help_text='Exposure time, in seconds.'
+    )
+
+    exposure_count = models.IntegerField(
+        verbose_name='Exposure Count', help_text='Number of exposures.'
+    )
+
+    # Boilerplate fields
+    created = models.DateTimeField(
+        auto_now_add=True, verbose_name='Time Created',
+        help_text='The time which this target was created in the TOM database.'
+    )
+    modified = models.DateTimeField(
+        auto_now=True, verbose_name='Last Modified',
+        help_text='The time which this target was changed in the TOM database.'
+    )

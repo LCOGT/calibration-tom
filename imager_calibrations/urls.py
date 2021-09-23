145cbf9c0b94a8e292a9b206e50a96b3cddea1bf
nvolgenau <nvolgenau@lcogt.net>
2021-09-23 23:13:04 +0000

    Create paths for ImagerCadenceView, ImagerCalibrationsView

--- imager_calibrations/urls.py
+++ imager_calibrations/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from imager_calibrations.views import ImagerCadenceView, ImagerCadenceSiteView
+from imager_calibrations.views import ImagerCadenceView, ImagerCadenceSiteView, ImagerCalibrationsView
 
 app_name = 'imager_calibrations'
 
 urlpatterns = [
-    path('list/', ImagerCadenceView.as_view(), name='imager_home'),
+    path('cadences/', ImagerCadenceView.as_view(), name='imager_home'),
+    path('targets/', ImagerCalibrationsView.as_view(), name='imager_home'),
     path('site/<str:site>/', ImagerCadenceSiteView.as_view(), name='site_detail')
 ]

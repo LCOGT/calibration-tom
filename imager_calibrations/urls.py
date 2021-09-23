2afcece4e485940940a74a114b19c5ac1b1d343a
nvolgenau <nvolgenau@lcogt.net>
2021-09-23 23:46:03 +0000

    Rename urlpattern names

--- imager_calibrations/urls.py
+++ imager_calibrations/urls.py
@@ -5,7 +5,7 @@ from imager_calibrations.views import ImagerCadenceView, ImagerCadenceSiteView, 
 app_name = 'imager_calibrations'
 
 urlpatterns = [
-    path('cadences/', ImagerCadenceView.as_view(), name='imager_home'),
-    path('targets/', ImagerCalibrationsView.as_view(), name='imager_home'),
+    path('cadences/', ImagerCadenceView.as_view(), name='imager_cadences'),
+    path('targets/', ImagerCalibrationsView.as_view(), name='imager_targets'),
     path('site/<str:site>/', ImagerCadenceSiteView.as_view(), name='site_detail')
 ]

e1c033c3bccc2904880f933528db5eca5ff9b8bb
nvolgenau <nvolgenau@lcogt.net>
2022-03-22 23:07:06 +0000

    Add tom_network to INSTALLED_APPS

--- calibration_tom/settings.py
+++ calibration_tom/settings.py
@@ -60,6 +60,7 @@ INSTALLED_APPS = [
     'tom_catalogs',
     'tom_observations',
     'tom_dataproducts',
+    'tom_network',
     'nres_calibrations',
     'photometric_standards',
     'calibrations',
@@ -302,6 +303,7 @@ OPEN_URLS = []
 
 HOOKS = {
     'target_post_save': 'tom_common.hooks.target_post_save',
+    'instrument_post_save': 'tom_common.hooks.instrument_post_save',
     # 'observation_change_state': 'tom_common.hooks.observation_change_state',
     'observation_change_state': 'calibrations.hooks.observation_change_state',
     'data_product_post_upload': 'tom_dataproducts.hooks.data_product_post_upload',

f10ddee7e6502581392e7816cddbf95da3794cda
nvolgenau <nvolgenau@lcogt.net>
2022-04-13 23:28:08 +0000

    Add BIAS_SITES

--- calibration_tom/settings.py
+++ calibration_tom/settings.py
@@ -321,6 +321,7 @@ THUMBNAIL_DEFAULT_SIZE = (200, 200)
 #CONFIGDB_URL = os.getenv('CONFIGDB_URL', 'http://configdb-dev.lco.gtn')
 CONFIGDB_URL = os.getenv('CONFIGDB_URL', 'http://configdb.lco.gtn')
 
+BIAS_SITES = ('coj', 'tlv', 'cpt', 'tfn', 'lsc', 'elp', 'ogg')
 PHOTOMETRIC_STANDARDS_SITES = ('coj', 'cpt', 'tfn', 'lsc', 'elp', 'ogg')
 
 NRES_SITES = ('cpt', 'tlv', 'lsc', 'elp')
